8a74dd07a4109f0b513eb192a39dfc29a333b0ee
abhiram304 <abhiram.304@gmail.com>
2017-06-11 06:46:00 -0700

    first commit

--- routes/index.js
+++ routes/index.js
@@ -7,17 +7,25 @@ var authToken = '2b15b2967968d5a4b6d0ddcefa3563b9';
 
 //require the Twilio module and create a REST client 
 var client = require('twilio')(accountSid, authToken); 
-
+var unirest = require('unirest');
 exports.index = function(req, res){
 	var text = req.body.Body;
 	console.log("Question revcieved with text: "+text);
-	client.messages.create({
-		to: "+15102039956", 
-		from: "+16579995558",
-		body: text,  
-	}, function(err, message) { 
-		console.log(message); 
-		res.render('index', { title: 'Express' });
-	});	
+	
+	unirest.get("https://webknox-question-answering.p.mashape.com/questions/answers?answerLookup=false&answerSearch=false&question="+text)
+	.header("X-Mashape-Key", "EnZHUmLCJSmshGfBoO1bl9JUT1LMp1weRDljsnnk6zBED4xWLJ")
+	.header("Accept", "application/json")
+	.end(function (result) {
+		client.messages.create({
+			to: "+15102039956", 
+			from: "+16579995558",
+			body: result.body,  
+		}, function(err, message) { 
+			console.log(message); 
+			res.render('index', { title: 'Express' });
+		});
+		console.log(result.status, result.headers, result.body);
+	});
+		
   
 };
