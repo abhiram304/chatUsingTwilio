194914b19bf138bdd80bb089d29dd78eddaba0da
abhiram304 <abhiram.304@gmail.com>
2017-06-11 08:30:04 -0700

    first commit

--- routes/index.js
+++ routes/index.js
@@ -8,24 +8,73 @@ var authToken = '2b15b2967968d5a4b6d0ddcefa3563b9';
 //require the Twilio module and create a REST client 
 var client = require('twilio')(accountSid, authToken); 
 var unirest = require('unirest');
+var yield = require('yield');
 exports.index = function(req, res){
 	var text = req.body.Body;
 	console.log("Question revcieved with text: "+text);
-	
-	unirest.get("https://webknox-question-answering.p.mashape.com/questions/answers?answerLookup=false&answerSearch=false&question="+text)
-	.header("X-Mashape-Key", "EnZHUmLCJSmshGfBoO1bl9JUT1LMp1weRDljsnnk6zBED4xWLJ")
-	.header("Accept", "application/json")
-	.end(function (result) {
-		client.messages.create({
+	var from = req.body.From;
+
+	//var request = require("request");
+	//var question = "https://webknox-question-answering.p.mashape.com/questions/answers?answerLookup=false&answerSearch=false&question="+text;
+	//var wolfram = require('wolfram-alpha').createClient("XKEPG9-PLE83Q7XAT",{output:"json"});
+
+	var Client = require('node-wolfram');
+	var Wolfram = new Client('XKEPG9-PLE83Q7XAT');
+	var ans = "Sorry I didn't find that";
+	Wolfram.query("American President", function(err, result) {
+		if(err)
+			console.log(err);
+		else
+		{
+			for(var a=0; a<result.queryresult.pod.length; a++)
+			{
+				var pod = result.queryresult.pod[a];
+				//console.log("SSS "+pod.$.title+" SSS",": ");
+				if(pod.$.title == "Result"){
+					for(var b=0; b<pod.subpod.length; b++)
+					{
+						var subpod = pod.subpod[b];
+						for(var c=0; c<subpod.plaintext.length; c++)
+						{
+							var text = subpod.plaintext[c];
+							ans = text;
+							console.log('\t', text);
+						}
+					}
+				}
+			}
+			client.messages.create({
+				to: "+15102039956", 
+				from: "+16579995558",
+				body:ans,  
+			}, function(err, message) { 
+				console.log(message); 
+				res.render('index', { title: 'Express' });
+			});
+		}
+	});	
+
+
+	/*request(question, function(error, response, body) {
+	  console.log(body);
+	  client.messages.create({
 			to: "+15102039956", 
 			from: "+16579995558",
-			body: result.body,  
+			body:body,  
 		}, function(err, message) { 
 			console.log(message); 
 			res.render('index', { title: 'Express' });
 		});
-		console.log(result.status, result.headers, result.body);
 	});
-		
-  
+	 */
+
+	/*unirest.get("https://webknox-question-answering.p.mashape.com/questions/answers?answerLookup=false&answerSearch=false&question="+text)
+	.header("X-Mashape-Key", "EnZHUmLCJSmshGfBoO1bl9JUT1LMp1weRDljsnnk6zBED4xWLJ")
+	.header("Accept", "application/json")
+	.end(function (result) {
+
+		console.log(result.status, result.headers, result.body);
+	});*/
+
+
 };
